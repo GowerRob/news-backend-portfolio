48dfd2abc4031fe875368fac895e01a4f22bf649
RobbieG <rwhgower63@gmail.com>
2023-10-02 12:13:18 +0100

    Minor correction to my earlier editing of seed( {}), back to original format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,9 +2,9 @@ const app = require('../app.js');
 const request = require('supertest');
 const db = require('../db/connection.js');
 const seed = require('../db/seeds/seed.js');
-const {articleData,commentData,topicData,userData}= require('../db/data/test-data/index.js');
+const testData= require('../db/data/test-data/index.js');
 
-beforeEach(()=>seed(topicData,userData,articleData,commentData));
+beforeEach(()=>seed(testData));
 afterAll(()=>db.end());
 
 describe('/api/topics',()=>{
