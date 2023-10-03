414e352f685926963dc2b820ab17bab93accc244
RobbieG <rwhgower63@gmail.com>
2023-10-03 10:37:17 +0100

    Amended DB query so that ALL articles are returned ; left join

--- db/models/models.js
+++ db/models/models.js
@@ -26,7 +26,7 @@ exports.fetchAllArticles=()=>{
     articles.created_at,articles.votes, 
     articles.article_img_url, COUNT(comments.comment_id) as comment_count
     FROM articles
-    JOIN comments On articles.article_id=comments.article_id
+    LEFT JOIN comments On articles.article_id=comments.article_id
     GROUP BY articles.article_id
     ORDER BY created_at DESC;
     `
